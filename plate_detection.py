eff8a413a2f397eecb6388520fdb5481b8befff6
Görkem <gorkemturkut@hotmail.com>
2024-07-25 14:38:56 +0300

    commit

--- plate_detection.py
+++ plate_detection.py
@@ -8,13 +8,22 @@ import numpy as np
 import re
 import sqlite3
 from datetime import datetime, timedelta
+import time
 
-# Establish connection to SQLite database
+# RTSP Camera Information
+USER_NAME = input("User Name: ")
+PASSWORD = input("Password: ")
+IP = "192.168.1.156"
+CONNECTION_POINT = "554"
+CHANNEL = "/ch1/main/av_stream"
+RTSP_URL = f'rtsp://{USER_NAME}:{PASSWORD}@{IP}:{CONNECTION_POINT}{CHANNEL}'
+
+# Connect to SQLite database
 db_file = 'license_plates.db'
 conn = sqlite3.connect(db_file)
 c = conn.cursor()
 
-# Create table if not exists
+# Create table if it doesn't exist
 c.execute('''
 CREATE TABLE IF NOT EXISTS plates (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,12 +39,12 @@ conn.commit()
 model_path = 'best.pt'
 model = YOLO(model_path)
 
-# Initialize the EasyOCR reader
+# Initialize EasyOCR reader
 reader = easyocr.Reader(['tr'])
 
-# Function to process images
+# Image processing function
 def process_image(img):
-    # Perform prediction using the model
+    # Make predictions using the model
     results = model(img)
 
     for result in results:
@@ -45,7 +54,7 @@ def process_image(img):
             # Draw the detected box
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)  # Increase thickness
 
-            # Crop the box and perform OCR
+            # Crop the box and apply OCR
             img_pil = Image.fromarray(img)
             cropped_image = img_pil.crop((x1, y1, x2, y2))
             image_np = np.array(cropped_image)
@@ -66,19 +75,19 @@ def process_image(img):
             result_text = result_text.replace(" ", "").upper()
             result_text = result_text.replace("-", "")
 
-            # Search for Turkish plate format
+            # Search for Turkish license plate format
             match = re.search(r'\d{2}[A-Z]{1,3}\d{2,4}', result_text)
 
-            if match and max_confidence > 0.8:
-                plaka = match.group(0)
+            if match and max_confidence > 0.75:
+                plate = match.group(0)
                 accuracy = int(max_confidence * 100)  # Convert to percentage
 
-                # Add the plate to the image
-                display_text = f"{plaka} %{accuracy}"
-                cv2.putText(img, display_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)  # font size and thickness
+                # Add plate to the image
+                display_text = f"{plate} %{accuracy}"
+                cv2.putText(img, display_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)  # Increase font size and thickness
 
                 # Check the plate in the database
-                c.execute("SELECT * FROM plates WHERE plate=? ORDER BY entry_time DESC LIMIT 1", (plaka,))
+                c.execute("SELECT * FROM plates WHERE plate=? ORDER BY entry_time DESC LIMIT 1", (plate,))
                 plate_record = c.fetchone()
 
                 current_time = datetime.now()
@@ -86,25 +95,25 @@ def process_image(img):
 
                 if plate_record is None:
                     # New plate, record entry time
-                    c.execute("INSERT INTO plates (plate, entry_time, exit_time) VALUES (?, ?, NULL)", (plaka, current_time_str))
-                    print(f"New plate detected: {plaka}, entry time: {current_time_str}")
+                    c.execute("INSERT INTO plates (plate, entry_time, exit_time) VALUES (?, ?, NULL)", (plate, current_time_str))
+                    print(f"New plate detected: {plate}, entry time: {current_time_str}")
                 else:
                     last_entry_time = datetime.strptime(plate_record[2], "%Y-%m-%d %H:%M:%S")
                     if plate_record[3] is None:
-                        # Exit time not yet recorded
+                        # Exit time not recorded yet
                         if current_time - last_entry_time > timedelta(minutes=2):
-                            c.execute("UPDATE plates SET exit_time=? WHERE plate=?", (current_time_str, plaka))
-                            print(f"Plate exited: {plaka}, exit time: {current_time_str}")
+                            c.execute("UPDATE plates SET exit_time=? WHERE plate=?", (current_time_str, plate))
+                            print(f"Plate exited: {plate}, exit time: {current_time_str}")
                         else:
-                            print(f"Plate {plaka} exit ignored due to 2-minute rule.")
+                            print(f"Plate {plate} exit ignored due to 2-minute rule.")
                     else:
                         # Exit time recorded, check for new entry
                         last_exit_time = datetime.strptime(plate_record[3], "%Y-%m-%d %H:%M:%S")
                         if current_time - last_exit_time > timedelta(minutes=2):
-                            c.execute("INSERT INTO plates (plate, entry_time, exit_time) VALUES (?, ?, NULL)", (plaka, current_time_str))
-                            print(f"Plate re-entered: {plaka}, entry time: {current_time_str}")
+                            c.execute("INSERT INTO plates (plate, entry_time, exit_time) VALUES (?, ?, NULL)", (plate, current_time_str))
+                            print(f"Plate re-entered: {plate}, entry time: {current_time_str}")
                         else:
-                            print(f"Plate {plaka} entry ignored due to 2-minute rule.")
+                            print(f"Plate {plate} entry ignored due to 2-minute rule.")
 
                 conn.commit()
 
@@ -113,7 +122,7 @@ def process_image(img):
 
     return img
 
-# Function to process videos
+# Video processing function
 def process_video(video_path, output_path):
     cap = cv2.VideoCapture(video_path)
 
@@ -123,7 +132,7 @@ def process_video(video_path, output_path):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # Start video writer
+    # Start the video writer
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
     while cap.isOpened():
@@ -146,17 +155,17 @@ def main(input_path, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Process input file based on its type
     if input_path.endswith(('.jpg', '.jpeg', '.png')):
         img = cv2.imread(input_path)
         output_path = os.path.join(output_dir, 'result.jpg')
         processed_img = process_image(img)
         cv2.imwrite(output_path, processed_img)
         plt.imshow(cv2.cvtColor(processed_img, cv2.COLOR_BGR2RGB))
-        # plt.show()
+        plt.show()
     elif input_path.endswith(('.mp4', '.avi', '.mov')):
         output_path = os.path.join(output_dir, 'result_video.mp4')
         process_video(input_path, output_path)
+
     elif input_path == 'camera':
         # Capture from camera
         cap = cv2.VideoCapture(0)
@@ -175,14 +184,43 @@ def main(input_path, output_dir):
             # Break loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
         cap.release()
         cv2.destroyAllWindows()
+
+    elif input_path.startswith('rtsp://'):
+        # Capture from camera stream
+        while True:
+            try:
+                cap = cv2.VideoCapture(input_path)
+                if not cap.isOpened():
+                    raise ValueError("Camera connection failed.")
+
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        raise ValueError("Frame could not be read, stream ended.")
+                    
+                    processed_frame = process_image(frame)
+                    cv2.imshow('Stream', processed_frame)
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                cap.release()
+                cv2.destroyAllWindows()
+                break  # Break loop to restart
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                cap.release()
+                cv2.destroyAllWindows()
+                print("Waiting 5 seconds and reconnecting...")
+                time.sleep(5)  # Wait 5 seconds and retry
 
     else:
         print("Unsupported input type.")
 
-# Example usage for camera capture
-input_path = 'testing_images/1.jpg'
+# Example usage for external camera stream
+input_path = RTSP_URL
 output_dir = 'output_directory'
 main(input_path, output_dir)
